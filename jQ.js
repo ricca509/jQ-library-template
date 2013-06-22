e5a03eb05501b1eee14efb6cb1f16221b0da4ece
Riccardo Coppola <coppola.riccardo@gmail.com>
2013-06-22 23:22:27 +0200

    Code commented and cleaned

--- jQ.js
+++ jQ.js
@@ -1,17 +1,22 @@
-var jQ = (function (window) {
+var jQ = (function (window, undefined) {
+    // This is the constructor function. It will be attached to the window object
+    // and executed every time we call jQ(...). Returns a new 'instance' of the library.
     var jQ = function (args) {
         return new jQ.fn.init(args);
     };
 
+    // Create the 'fn' object which is the same as 'prototype' to enable a simpler way to extend the library
     jQ.fn = jQ.prototype = {
         // Core functions
+        // Add all core functions here, be sure to return 'this' to enable cascading
         init: function (args) {
-            console.log('init called');
-
-            if (!args) {
+            // Check args and initialize the local variables attaching them to 'this'
+            if (args === undefined) {
                 return this;
             } else {
                 this.args = args;
+
+                return this;
             }
         },
         test: function () {
@@ -26,6 +31,7 @@ var jQ = (function (window) {
     // I return has all the methods declared
     jQ.fn.init.prototype = jQ.fn;
 
+    // Attach the constructor function to the window object
     return window.jQ = jQ;
 })(window);
 
